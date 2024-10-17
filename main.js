c1bf9b53b0e45ed960b53203dcb330f13adca00f
Behindtheboard <jk2624@gmail.com>
2024-10-16 19:48:54 -0700

    Update Main

--- main.js
+++ main.js
@@ -1,4 +1,23 @@
 import { linkedList } from "./linked-list.js";
-import { node } from "./node.js"
 
-
+let list = linkedList();
+list.append("dog");
+list.append("cat");
+list.append("parrot");
+list.append("hamster");
+list.append("snake");
+list.append("turtle");
+console.log(list.getSize());
+console.log(list.getHead());
+console.log(list.getTail());
+console.log(list.at(4));
+console.log(list.pop());
+console.log(list.getSize());
+console.log(list.getHead());
+console.log(list.contains("dog"));
+console.log(list.find("snake"));
+console.log(list.toString());
+console.log(list.insertAt("monkey", 4));
+console.log(list.toString());
+console.log(list.removeAt(4))
+console.log(list.toString());
